1853d27a2dc50a17702ff3b6897e92e477eb75c3
Czesio <grzesiek_czesio@o2.pl>
2016-05-15 14:29:44 +0200

    Fixed build script

--- build.ps1
+++ build.ps1
@@ -1,16 +1,16 @@
 $Location = Get-Location
 $Configuration = "Release"
 
-# run the build
+# Run the build
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" src\Luma.SmartHub.sln /property:Configuration=$Configuration
 
-// Search for all nuspec files
+# Search for all nuspec files
 Get-ChildItem -r *.nuspec | ForEach-Object {
 
-    // Finding csproj files in directory with nuspec file
+    # Finding csproj files in directory with nuspec file
     Get-ChildItem $_.Directory -r *.csproj| ForEach-Object {
 
-        // Packaging project to nupkg
+        # Packaging project to nupkg
         Write-Host Packaging $_.FullName
         NuGet Pack $_.FullName -Prop Configuration=$Configuration
     }
